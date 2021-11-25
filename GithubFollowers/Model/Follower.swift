0789896918fdf020b9704c7c5285ae96a0ae5164
tonivegaochoa <tonivegaochoa@gmail.com>
2021-11-25 15:30:55 -0600

    added Follower and User models for network calls

---
+++ GithubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Antonio Vega on 11/25/21.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
