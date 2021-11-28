73a6b310141354e3c84facacafa9f32cd28d2868
tonivegaochoa <tonivegaochoa@gmail.com>
2021-11-27 18:15:34 -0600

    added GFUserInfoHeaderVC

---
+++ GithubFollowers/Custom Views/Labels/GFSecondayTitleLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GFSecondayTitleLabel.swift
+//  GithubFollowers
+//
+//  Created by Antonio Vega on 11/27/21.
+//
+
+import UIKit
+
+class GFSecondayTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
---
+++ GithubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GithubFollowers
+//
+//  Created by Antonio Vega on 11/27/21.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
